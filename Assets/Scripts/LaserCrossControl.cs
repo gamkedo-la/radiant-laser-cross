ca8ad36aec497a1054d937d27381126820ff857c
A. Joël Lamotte (Klaim) <mjklaim@gmail.com>
2018-09-10 01:29:15 +0200

    Initial state of the project: Just the RLC structured and placed on the screen.

---
+++ Assets/Scripts/LaserCrossControl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class LaserCrossControl : MonoBehaviour {
+
+
+    // Use this for initialization
+    void Start () {
+
+    }
+
+    private void FixedUpdate()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+}
+
