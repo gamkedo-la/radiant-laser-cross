1911e7b518b16479f7d10a50a3570c8af03be62b
A. Joël Lamotte (Klaim) <mjklaim@gmail.com>
2018-09-10 21:03:29 +0200

    Laser Cross gun rotation now locks in axis. Fun fun fun!

--- Assets/Scripts/rlc/LaserCross.cs
+++ Assets/Scripts/rlc/LaserCross.cs
@@ -20,6 +20,18 @@ namespace rlc
         public const float MAX_GUNS_ORIENTATION_DEGREES = 360.0f;
         public const float GUNS_DEGREES_PER_DIRECTION = MAX_GUNS_ORIENTATION_DEGREES / GUNS_COUNT;
         public const float HALF_GUNS_DEGREES_PER_DIRECTION = GUNS_DEGREES_PER_DIRECTION / 2;
+        // Keep in mind north direction is between the highest and lowest values.
+        private const float begin_east_angle = HALF_GUNS_DEGREES_PER_DIRECTION;
+        private const float end_east_angle = begin_east_angle + GUNS_DEGREES_PER_DIRECTION;
+        private const float begin_south_angle = end_east_angle;
+        private const float end_south_angle = begin_south_angle + GUNS_DEGREES_PER_DIRECTION;
+        private const float begin_west_angle = end_south_angle;
+        private const float end_west_angle = begin_west_angle + GUNS_DEGREES_PER_DIRECTION;
+        private const float left_side_north_begin_angle = end_west_angle;
+        private const float right_side_north_end_angle = begin_east_angle;
+
+
+
 
         private LaserCrossGun[] guns = new LaserCrossGun[GUNS_COUNT];
         private float current_guns_angle = 0.0f;
@@ -42,17 +54,17 @@ namespace rlc
             guns = GetComponentsInChildren<LaserCrossGun>();
         }
 
-        public void push_next_commands(Commands commands)
-        {
-            next_commands = commands;
-        }
-
-
         void Update()
         {
             apply_commands(next_commands);
             clear_commands();
         }
+
+        public void push_next_commands(Commands commands)
+        {
+            next_commands = commands;
+        }
+
 
         private void apply_commands(Commands commands)
         {
@@ -83,7 +95,10 @@ namespace rlc
             // If the player maintain a rotation direction, we should seemlessly continue
             // rotating until they don't, then we stop rotation once reaching an axis orientation.
 
-            current_guns_rotation = rotation_from_commands; // TEMPORARY
+            if (current_guns_rotation == GunsRotation.none) // If we are already in locked position, take any command, otherwise ignore commands until next lock position.
+            {
+                current_guns_rotation = rotation_from_commands;
+            }
 
             float rotation_factor = gun_rotation_factor(current_guns_rotation);
             float next_orientation = (current_guns_angle + rotation_speed * Time.deltaTime * rotation_factor);
@@ -95,14 +110,30 @@ namespace rlc
 
             var new_rotation_direction = direction(next_orientation);
 
-            // ... TODO: lock missing here
+            var new_target_axis = next_axis(next_orientation, current_guns_rotation);
+            var current_target_axis = next_axis(current_guns_angle, current_guns_rotation);
+            if (new_target_axis != current_target_axis)
+            {
+                // We are passing over/through a lock position.
+                // We either stop here or change direction following commands.
+                // It all depends on what is the command from input.
+                current_guns_rotation = rotation_from_commands;
+                if (current_guns_rotation == GunsRotation.none)
+                {
+                    // We stopped: lock the position to align with axis.
+                    next_orientation = angle(current_target_axis);
+                }
+            }
 
+            // Handle gun firing rotation: the firing direction gives the closest gun to that direction to fire.
             if (new_rotation_direction != current_guns_directions)
             {
                 // We will change of gun direction.
                 rotate_guns_directions((int)rotation_factor);
                 current_guns_directions = new_rotation_direction;
             }
+
+            // Apply guns rotation.
             current_guns_angle = next_orientation;
             transform.rotation = Quaternion.Euler(0.0f, 0.0f, -current_guns_angle); // Because of Z being oriented this way in Unity, we need negative values to rotate clockwise.
         }
@@ -112,29 +143,56 @@ namespace rlc
            Utility.Rotate(guns, rotation_steps);
         }
 
-        private bool can_change_gun_rotation_direction()
+        private static GunFireDirection next_axis(float current_angle, GunsRotation rotation)
         {
-            return true;
+            if (rotation == GunsRotation.none)
+                return direction(current_angle);
+
+            if (rotation == GunsRotation.rotate_clockwise)
+            {
+                if (current_angle > angle(GunFireDirection.west))
+                    return GunFireDirection.north;
+                if (current_angle > angle(GunFireDirection.south))
+                    return GunFireDirection.west;
+                if (current_angle > angle(GunFireDirection.east))
+                    return GunFireDirection.south;
+                return GunFireDirection.east;
+            }
+            else
+            {
+                if (current_angle < angle(GunFireDirection.east))
+                    return GunFireDirection.north;
+                if (current_angle < angle(GunFireDirection.south))
+                    return GunFireDirection.east;
+                if (current_angle < angle(GunFireDirection.west))
+                    return GunFireDirection.south;
+                return GunFireDirection.west;
+            }
+        }
+
+
+        private static float angle(GunFireDirection direction)
+        {
+            switch (direction)
+            {
+                case GunFireDirection.north:
+                    return 0.0f;
+                case GunFireDirection.east:
+                    return GUNS_DEGREES_PER_DIRECTION;
+                case GunFireDirection.south:
+                    return GUNS_DEGREES_PER_DIRECTION * 2;
+                case GunFireDirection.west:
+                    return GUNS_DEGREES_PER_DIRECTION * 3;
+                default:
+                    Assert.IsTrue(false);
+                    return 0.0f;
+            }
         }
 
         private static GunFireDirection direction(float angle_deg)
         {
             Assert.IsTrue(angle_deg >= 0);
             Assert.IsTrue(angle_deg <= MAX_GUNS_ORIENTATION_DEGREES);
-
-            // Keep in mind north direction is between the highest and lowest values.
-            const float begin_east_angle = HALF_GUNS_DEGREES_PER_DIRECTION;
-            const float end_east_angle = begin_east_angle + GUNS_DEGREES_PER_DIRECTION;
-
-            const float begin_south_angle = end_east_angle;
-            const float end_south_angle = begin_south_angle + GUNS_DEGREES_PER_DIRECTION;
-
-            const float begin_west_angle = end_south_angle;
-            const float end_west_angle = begin_west_angle + GUNS_DEGREES_PER_DIRECTION;
-
-            const float left_side_north_begin_angle = end_west_angle;
-            const float right_side_north_end_angle = begin_east_angle;
-
 
 
             if (angle_deg >= left_side_north_begin_angle || angle_deg < right_side_north_end_angle)
