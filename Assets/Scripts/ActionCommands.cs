36bdaff6e6201a920886545bb98685812f42057e
A. Joël Lamotte (Klaim) <mjklaim@gmail.com>
2018-09-10 12:56:20 +0200

    Laser Cross now moves when using keyboard arrows. Because of a weird latency issue I couldn't use the generic way to get input so I focused on just moving with direct keycodes for now.

---
+++ Assets/Scripts/ActionCommands.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace rlc
+{
+
+    public enum GunMoveAction // Only one can be used at a time.
+    {
+        none,
+        rotate_clockwise,
+        rotate_counter_clockwise
+    }
+
+    public enum GunTriggerAction // All can be used simultaneously.
+    {
+        none,
+        fire_north,
+        fire_east,
+        fire_south,
+        fire_west,
+    }
+
+
+    // Command to execute to control Laser Cross for one update cycle
+    public struct Commands
+    {
+        const int MAX_MOVE_ACTIONS_PER_ACTION = 2;
+        public Vector2 ship_direction;               // Normalized direction to where the ship needs to go.
+        public GunMoveAction gun_move;               // Gun rotation to engage.
+        public GunTriggerAction gun_trigger;         // Guns to trigger for shooting.
+    }
+
+
+}
--- Assets/Scripts/LaserCrossControl.cs
+++ Assets/Scripts/LaserCrossControl.cs
@@ -2,23 +2,57 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+namespace rlc
+{
 
-public class LaserCrossControl : MonoBehaviour {
-
-
-    // Use this for initialization
-    void Start () {
-
-    }
-
-    private void FixedUpdate()
+    public class LaserCrossControl : MonoBehaviour
     {
 
+        public LaserCross laser_cross;
+
+        void Start()
+        {
+
+        }
+
+        private void FixedUpdate()
+        {
+
+        }
+
+        void Update()
+        {
+            Commands commands = read_input();
+            laser_cross.push_next_commands(commands);
+        }
+
+
+        private Commands read_input()
+        {
+            Commands commands = new Commands();
+
+            // The code commented below generate latency between input and move and I'm not sure why:
+            //float vertical_move = Input.GetAxis("Vertical");
+            //float horizontal_move = Input.GetAxis("Horizontal");
+
+            //commands.ship_direction.y = vertical_move;
+            //commands.ship_direction.x = horizontal_move;
+
+            if (Input.GetKey(KeyCode.LeftArrow))
+                commands.ship_direction.x -= 1;
+            if (Input.GetKey(KeyCode.RightArrow))
+                commands.ship_direction.x += 1;
+            if (Input.GetKey(KeyCode.UpArrow))
+                commands.ship_direction.y += 1;
+            if (Input.GetKey(KeyCode.DownArrow))
+                commands.ship_direction.y -= 1;
+
+
+            return commands;
+        }
+
+
     }
 
-    // Update is called once per frame
-    void Update () {
 
-    }
 }
-
