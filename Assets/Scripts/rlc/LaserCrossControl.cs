62d8fa70cc538f74987bf0e28f98b8122486bece
A. Joël Lamotte (Klaim) <mjklaim@gmail.com>
2018-10-04 00:44:00 +0200

    Changed input control to use unity's input manager -> allow gamepads but gamepad have not been tested yet!

--- Assets/Scripts/rlc/LaserCrossControl.cs
+++ Assets/Scripts/rlc/LaserCrossControl.cs
@@ -28,34 +28,61 @@ namespace rlc
             // OK Chris said I should use GetAxisRaw instead and it works but then I'm not sure
             // How to setup the rotations and 4 guns firing in Unity.
 
-            //float vertical_move = Input.GetAxis("Vertical");
-            //float horizontal_move = Input.GetAxis("Horizontal");
+            float vertical_move = Input.GetAxisRaw("Vertical");
+            float horizontal_move = Input.GetAxisRaw("Horizontal");
+            float fire_north = Input.GetAxisRaw("Fire Up");
+            float fire_east = Input.GetAxisRaw("Fire Right");
+            float fire_south = Input.GetAxisRaw("Fire Down");
+            float fire_west = Input.GetAxisRaw("Fire Left");
+            float rotate_clockwise = Input.GetAxisRaw("Rotate Clockwise");
+            float rotate_counterclockwise = Input.GetAxisRaw("Rotate Counter-Clockwise");
 
-            //commands.ship_direction.y = vertical_move;
-            //commands.ship_direction.x = horizontal_move;
+            if (vertical_move > 0)
+                commands.ship_direction.y = +1;
+            if (vertical_move < 0)
+                commands.ship_direction.y = -1;
 
-            if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.Keypad4))
-                commands.ship_direction.x -= 1;
-            if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.Keypad6))
-                commands.ship_direction.x += 1;
-            if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.Keypad8))
-                commands.ship_direction.y += 1;
-            if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.Keypad5))
-                commands.ship_direction.y -= 1;
+            if (horizontal_move > 0)
+                commands.ship_direction.x = +1;
+            if (horizontal_move < 0)
+                commands.ship_direction.x = -1;
 
-            if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.J))
+            if (fire_west > 0)
                 commands.gun_trigger.fire_west = true;
-            if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.L))
+            if (fire_east > 0)
                 commands.gun_trigger.fire_east = true;
-            if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.I))
+            if (fire_north > 0)
                 commands.gun_trigger.fire_north = true;
-            if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.K))
+            if (fire_south > 0)
                 commands.gun_trigger.fire_south = true;
 
-            if (Input.GetKey(KeyCode.Q) || Input.GetKey(KeyCode.Keypad7))
+            if (rotate_counterclockwise > 0)
                 commands.gun_move = GunsRotation.rotate_counter_clockwise;
-            if (Input.GetKey(KeyCode.E) || Input.GetKey(KeyCode.Keypad9))
+            if (rotate_clockwise > 0)
                 commands.gun_move = GunsRotation.rotate_clockwise;
+
+            //if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.Keypad4))
+            //    commands.ship_direction.x -= 1;
+            //if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.Keypad6))
+            //    commands.ship_direction.x += 1;
+            //if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.Keypad8))
+            //    commands.ship_direction.y += 1;
+            //if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.Keypad5))
+            //    commands.ship_direction.y -= 1;
+
+            //if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.J))
+            //    commands.gun_trigger.fire_west = true;
+            //if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.L))
+            //    commands.gun_trigger.fire_east = true;
+            //if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.I))
+            //    commands.gun_trigger.fire_north = true;
+            //if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.K))
+            //    commands.gun_trigger.fire_south = true;
+
+            //if (Input.GetKey(KeyCode.Q) || Input.GetKey(KeyCode.Keypad7))
+            //    commands.gun_move = GunsRotation.rotate_counter_clockwise;
+            //if (Input.GetKey(KeyCode.E) || Input.GetKey(KeyCode.Keypad9))
+            //    commands.gun_move = GunsRotation.rotate_clockwise;
 
             return commands;
         }
