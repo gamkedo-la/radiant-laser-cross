9d3359b982ef63b5670cb6327b48f2c0cd51173d
A. Joël Lamotte (Klaim) <mjklaim@gmail.com>
2018-10-28 03:15:48 +0100

    Added a random twisty movement mainly used for animation.

---
+++ Assets/Scripts/rlc/movement/TwistyTwisting.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TwistyTwisting : MonoBehaviour {
+
+    // Use this for initialization
+    void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        // Copy pasted from the "temporary" death animation of the laser cross, then tweaked to be realy random.
+        transform.rotation = transform.rotation * Quaternion.Euler(Random.Range(1.0f, 1000.0f) * Time.deltaTime, Random.Range(1.0f, 1000.0f) * Time.deltaTime, Random.Range(1.0f, 1000.0f) * Time.deltaTime);
+    }
+}
