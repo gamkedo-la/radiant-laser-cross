7835e682e29101a326d02fbaa5b7fc98efa3849c
A. Joël Lamotte (Klaim) <mjklaim@gmail.com>
2018-10-28 19:34:20 +0100

    Added "Squarey Square" kinds of simple enemies from the tutorial.

---
+++ Assets/Scripts/enemies/Enemy_Squarey.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Squarey : MonoBehaviour {
+
+    public float speed = 5.0f;
+
+    // Use this for initialization
+    void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        rlc.Movement.move_forward(transform, speed);
+    }
+}
---
+++ Assets/Scripts/rlc/movement/Rotating.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotating : MonoBehaviour {
+
+    public float rotation_speed = 45.0f;
+
+    // Use this for initialization
+    void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        transform.Rotate(Vector3.up, rotation_speed * Time.deltaTime);
+    }
+}
