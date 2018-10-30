215fd82e247e0ee68c3f2ad2529ca60a70ddb382
A. Joël Lamotte (Klaim) <mjklaim@gmail.com>
2018-10-30 14:28:20 +0100

    Added new enemies that I made in the Part 2 and Part 3 of the enemy tutorial.

--- Assets/Scripts/enemies/Enemy_Squarey.cs
+++ Assets/Scripts/enemies/Enemy_Squarey.cs
@@ -6,13 +6,20 @@ public class Enemy_Squarey : MonoBehaviour {
 
     public float speed = 5.0f;
 
+    private List<rlc.Gun> guns = new List<rlc.Gun>();
+
     // Use this for initialization
     void Start () {
-
+        guns.AddRange(GetComponentsInChildren<rlc.Gun>());
     }
 
     // Update is called once per frame
     void Update () {
         rlc.Movement.move_forward(transform, speed);
+
+        foreach (var gun in guns)
+        {
+            gun.fire();
+        }
     }
 }
