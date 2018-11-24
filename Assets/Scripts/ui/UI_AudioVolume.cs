9ba1268cd25e5f6782688eb95df61a1035ae8a46
A. Joël Lamotte (Klaim) <mjklaim@gmail.com>
2018-11-24 18:53:40 +0100

    Added Pagup/down to change audio volume which is now displayed.

---
+++ Assets/Scripts/ui/UI_AudioVolume.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UI_AudioVolume : MonoBehaviour
+{
+
+    Text display;
+
+    // Use this for initialization
+    void Start()
+    {
+        display = GetComponent<Text>();
+        update_display(AudioListener.volume);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        const float volume_change = 0.1f;
+        if (Input.GetKeyDown(KeyCode.PageUp))
+        {
+            change_volume(+volume_change);
+
+        }
+        if (Input.GetKeyDown(KeyCode.PageDown))
+        {
+            change_volume(-volume_change);
+        }
+
+    }
+
+    private void change_volume(float change_value)
+    {
+        AudioListener.volume += change_value;
+        update_display(AudioListener.volume);
+    }
+
+    private void update_display(float new_volume)
+    {
+        const string display_format = "Audio Volume: {0}%";
+
+        int volume_percent = (int)(100 * new_volume);
+
+        var new_text = string.Format(display_format, volume_percent);
+
+        display.text = new_text;
+    }
+}
