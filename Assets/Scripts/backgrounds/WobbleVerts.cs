6d5d312a0bf74be7a21fc1d92ded9582d1630e0f
McFunkypants <breakdance@mcfunkypants.com>
2018-11-26 18:22:45 -0800

    Background3D prefab and game camera perspective mode w/ estimated angles - WIP

--- Assets/Scripts/backgrounds/WobbleVerts.cs
+++ Assets/Scripts/backgrounds/WobbleVerts.cs
@@ -7,6 +7,7 @@ public class WobbleVerts : MonoBehaviour
     public float speed = 1.0f;
     public float noiseStrength = 1f;
     public float noiseWalk = 1f;
+    public float z_strength = 0f;
 
     private Vector3[] baseHeight;
     private Mesh mesh;
@@ -28,6 +29,11 @@ public class WobbleVerts : MonoBehaviour
             vertex.x += Mathf.PerlinNoise(baseHeight[i].x + noiseWalk, baseHeight[i].y + Mathf.Sin(Time.time * 0.1f)) * noiseStrength;
             vertex.y += Mathf.Sin(Time.time * speed + baseHeight[i].x + baseHeight[i].y + baseHeight[i].z) * scale/2;
             vertex.y += Mathf.PerlinNoise(baseHeight[i].x, baseHeight[i].y + Mathf.Sin(Time.time * 0.1f)) * noiseStrength;
+            if (z_strength>0f)
+            {
+                vertex.y += Mathf.Sin(1234f + Time.time * speed + baseHeight[i].x + baseHeight[i].y + baseHeight[i].z) * scale / 2;
+                vertex.y += Mathf.PerlinNoise(baseHeight[i].x, baseHeight[i].y + Mathf.Sin(1234f + Time.time * 0.1f)) * z_strength;
+            }
             vertices[i] = vertex;
         }
 
