8bb14a82fe257caf8a01a1c15eafb998d3fc11ea
A. Joël Lamotte (Klaim) <mjklaim@gmail.com>
2018-12-02 17:49:28 +0100

    Fixed: d-pad being too precise.

--- Assets/Scripts/SpecialCommandHandler.cs
+++ Assets/Scripts/SpecialCommandHandler.cs
@@ -4,52 +4,70 @@ using UnityEngine;
 
 public class SpecialCommandHandler : MonoBehaviour {
 
-    rlc.ProceduralLevelBuilder level_builder;
+    private rlc.ProceduralLevelBuilder level_builder;
+    private float last_wave_change_time;
+    private const float time_between_wave_change = 1.0f;
+    private bool deactivate_input = false;
 
     // Use this for initialization
     void Start () {
         level_builder = GetComponentInChildren<rlc.ProceduralLevelBuilder>();
+        last_wave_change_time = Time.time;
     }
 
     // Update is called once per frame
     void Update () {
-        if (Input.GetKeyUp(KeyCode.Escape) ||
-            //Input.GetKeyDown("joystick 1 button 4")) // gamepad LB
-            (Input.GetAxis("DPAD_HORIZ") < -0.5)) // DPAD left
+
+        if (deactivate_input)
+            return;
+
+        var now = Time.time;
+        var time_since_last_wave_change = now - last_wave_change_time;
+
+        if (Input.GetKeyUp(KeyCode.Escape)
+        ||  Input.GetAxisRaw("DPAD_HORIZ") == -1) // DPAD left
         {
+            deactivate_input = true;
             level_builder.exit();
         }
 
-        if (Input.GetKeyUp(KeyCode.Space) ||
-            Input.GetKeyDown("joystick 1 button 7")) // gamepad start button
+        if (Input.GetKeyUp(KeyCode.Space)
+        ||  Input.GetKeyDown("joystick 1 button 7")) // gamepad start button
         {
             level_builder.new_game();
         }
 
         if (Input.GetKeyUp(KeyCode.R) ||
-            //Input.GetKeyDown("joystick 1 button 4")) // gamepad LB
             Input.GetKeyDown("joystick 1 button 6")) // gamepad back button
         {
-            level_builder.game_over(rlc.GameOverReason.hard_reset);
-        }
-
-        if (Input.GetKeyUp(KeyCode.Tab) ||
-            //Input.GetKeyDown("joystick 1 button 5")) // gamepad RB
-            (Input.GetAxis("DPAD_HORIZ") > 0.5)) // DPAD right
-        {
-            level_builder.next_wave();
-            rlc.Bullet.clear_bullets_from_game();
-        }
-
-         
-        // Gamepad Debug [to figure out which button # is which]
-        for (int i = 0; i < 20; i++)
-        {
-            if (Input.GetKeyDown("joystick 1 button " + i))
+            if (time_since_last_wave_change >= time_between_wave_change)
             {
-                print("joystick 1 button " + i + " pressed");
+                last_wave_change_time = Time.time;
+                level_builder.game_over(rlc.GameOverReason.hard_reset);
             }
         }
+
+        if (Input.GetKeyUp(KeyCode.Tab)
+        ||  Input.GetAxis("DPAD_HORIZ") == 1) // DPAD right
+        {
+
+            if (time_since_last_wave_change >= time_between_wave_change)
+            {
+                last_wave_change_time = Time.time;
+                level_builder.next_wave();
+                rlc.Bullet.clear_bullets_from_game();
+            }
+        }
+
+        // Reactivate this for debugging
+        // Gamepad Debug [to figure out which button # is which]
+        //for (int i = 0; i < 20; i++)
+        //{
+        //    if (Input.GetKeyDown("joystick 1 button " + i))
+        //    {
+        //        print("joystick 1 button " + i + " pressed");
+        //    }
+        //}
 
     }
 }
--- Assets/Scripts/ui/UI_AudioVolume.cs
+++ Assets/Scripts/ui/UI_AudioVolume.cs
@@ -5,27 +5,30 @@ using System.Collections;
 public class UI_AudioVolume : MonoBehaviour
 {
 
-    Text display;
+    private Text display;
+    private float last_volume_change;
+    const float wait_time_before_next_volume_change = 1.0f / 8.0f;
 
     // Use this for initialization
     void Start()
     {
         display = GetComponent<Text>();
         update_display(AudioListener.volume);
+        last_volume_change = Time.time;
     }
 
     // Update is called once per frame
     void Update()
     {
         const float volume_change = 0.1f;
-        if (Input.GetKeyDown(KeyCode.PageUp) ||
-            (Input.GetAxis("DPAD_VERT") > 0.5)) // DPAD up
+        if (Input.GetKeyDown(KeyCode.PageUp)
+        ||  Input.GetAxis("DPAD_VERT") == 1) // DPAD up
         {
             change_volume(+volume_change);
 
         }
-        if (Input.GetKeyDown(KeyCode.PageDown) ||
-            (Input.GetAxis("DPAD_VERT") < -0.5)) // DPAD down
+        if (Input.GetKeyDown(KeyCode.PageDown)
+        ||  Input.GetAxis("DPAD_VERT") == -1) // DPAD down
         {
             change_volume(-volume_change);
         }
@@ -34,8 +37,14 @@ public class UI_AudioVolume : MonoBehaviour
 
     private void change_volume(float change_value)
     {
-        AudioListener.volume += change_value;
-        update_display(AudioListener.volume);
+        var now = Time.time;
+        var time_since_last_volume_change = now - last_volume_change;
+        if (time_since_last_volume_change >= wait_time_before_next_volume_change)
+        {
+            last_volume_change = now;
+            AudioListener.volume += change_value;
+            update_display(AudioListener.volume);
+        }
     }
 
     private void update_display(float new_volume)
