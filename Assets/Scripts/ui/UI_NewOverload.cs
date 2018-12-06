0402b62b2e15858641bc9209371b2b84f136861a
hugolnx <hugolnx@gmail.com>
2018-12-06 21:07:47 -0200

    NewUI: Overload lamps with colors.

--- Assets/Scripts/ui/UI_NewOverload.cs
+++ Assets/Scripts/ui/UI_NewOverload.cs
@@ -4,16 +4,19 @@ using UnityEngine;
 
 public class UI_NewOverload : MonoBehaviour {
     private UI_OverloadBar bar;
+    private UI_OverloadLamps lamps;
     public UI_OverloadBarColor[] section_colors;
     private float last_level = 0f;
     private int last_section = 0;
     private int MAX_SECTIONS;
     private float MAX_RELATIVE_LEVEL;
+    public static Color REST_COLOR = new Color(0xE4, 0xFF, 0xFF);
 
     void Start () {
         MAX_SECTIONS = section_colors.Length / 2;
         MAX_RELATIVE_LEVEL = 100f / MAX_SECTIONS;
         bar = GetComponentInChildren<UI_OverloadBar>();
+        lamps = GetComponentInChildren<UI_OverloadLamps>();
         OverloadEvents.OnLoadChange += set_overload;
 	}
 
@@ -33,7 +36,16 @@ public class UI_NewOverload : MonoBehaviour {
         var ui_level = 60f * (level_section / (MAX_SECTIONS - 1f)) + 40f * (relative_level / MAX_RELATIVE_LEVEL);
         bar.disturbance = Mathf.RoundToInt(50f * (ui_level / 100f) + (is_increasing ? 50f : 0f));
         bar.level = Mathf.RoundToInt(ui_level);
-        
+        lamps.set_intensity(relative_level / MAX_RELATIVE_LEVEL);
+
+        if (load <= 0.01f)
+        {
+            lamps.change_color(REST_COLOR);
+        } else
+        {
+            lamps.change_color(section_colors[level_section * 2]);
+        }
+
         last_level = level;
     }
 }
---
+++ Assets/Scripts/ui/UI_OverloadLamps.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class UI_OverloadLamps : MonoBehaviour {
+    private Image image;
+    private float intensity = 0f;
+    private UI_OverloadBarColor color;
+    private Nullable<Color> force_color;
+
+    void Start()
+    {
+        image = GetComponent<Image>();
+        force_color = UI_NewOverload.REST_COLOR;
+    }
+
+    void Update()
+    {
+        if (force_color.HasValue)
+        {
+            image.color = force_color.Value;
+            return;
+        }
+
+        if (this.intensity <= 1f / 3f)
+        {
+            image.color = color.weak;
+        }
+        else if (this.intensity <= 1f / 3f * 2f)
+        {
+            image.color = color.medium;
+        }
+        else
+        {
+            image.color = color.strong;
+        }
+    }
+
+    public void change_color(Color color)
+    {
+        this.force_color = color;
+    }
+
+    public void change_color(UI_OverloadBarColor color)
+    {
+        this.color = color;
+        force_color = null;
+    }
+
+    public void set_intensity(float intensity)
+    {
+        this.intensity = intensity;
+    }
+}
