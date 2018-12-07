2252f498640a9d68d720681f35e1e943da8791ec
hugolnx <hugolnx@gmail.com>
2018-12-06 22:07:35 -0200

    NewUI: Overload rings lighting interface

---
+++ Assets/Scripts/ui/UI_OverloadRings.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_OverloadRings : MonoBehaviour {
+    private UI_OverloadRing[] rings;
+    public UI_OverloadBarColor[] colors;
+    public float disturbance;
+    private float animation_timeout = 0f;
+    private const float MAX_TIMEOUT = 0.1f;
+    private const float MIN_TIMEOUT = 0.01f;
+    private const int MIN_RINGS_AMOUNT = 1;
+
+
+    void Start () {
+        rings = GetComponentsInChildren<UI_OverloadRing>();
+	}
+	
+	void Update () {
+        animation_timeout += Time.deltaTime;
+        if (disturbance == 0) return;
+        if(animation_timeout >= Timeout)
+        {
+            animation_timeout = 0f;
+            if (colors.Length == 0) return;
+            var color = colors[Random.Range(0, colors.Length)];
+            var ring = rings[rings.Length - 1 - Random.Range(0, RingsAmount)];
+            ring.fill(color);
+        }
+	}
+
+    public float Timeout
+    {
+        get { return MAX_TIMEOUT - (MAX_TIMEOUT - MIN_TIMEOUT) * disturbance; }
+    }
+
+    public int RingsAmount
+    {
+        get { return MIN_RINGS_AMOUNT + Mathf.CeilToInt((rings.Length - MIN_RINGS_AMOUNT) * disturbance); }
+    }
+}
