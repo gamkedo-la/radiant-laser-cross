da6129d7aa2cf713ceb16d1383d951db9bef459d
A. Joël Lamotte (Klaim) <mjklaim@gmail.com>
2018-12-13 00:07:51 +0100

    Plugged sounds in main menu (except change button) and made sure we have time to hear them but still not be able to press more than one button.

--- Assets/Scripts/menus/MainMenu.cs
+++ Assets/Scripts/menus/MainMenu.cs
@@ -1,31 +1,79 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour {
+public class MainMenu : MonoBehaviour
+{
+
+    public AudioSource menu_change_sound;
+    public AudioSource menu_validate_sound;
+    public AudioSource menu_cancel_sound;
+
+
+    public List<Button> all_buttons;
+
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
 
     }
 
     void Update()
     {
         if (Input.GetKeyUp(KeyCode.Escape))
-            Application.Quit(); // FIXME: might cause problems in web builds
+        {
+            StartCoroutine(run_exit());
+        }
     }
 
     public void start_new_game()
     {
-        SceneManager.LoadScene("Main", LoadSceneMode.Single);
+        StartCoroutine(load_new_game()); ;
     }
 
 
     public void display_credits()
     {
-        SceneManager.LoadScene("Credits", LoadSceneMode.Single);
+        StartCoroutine(load_credits());
     }
 
+    private void deactivate_all_buttons()
+    {
+        foreach (var button in all_buttons)
+        {
+            button.gameObject.SetActive(false);
+        }
+    }
+
+    private const float seconds_to_next_screen = 1.0f;
+
+    private IEnumerator run_exit()
+    {
+        deactivate_all_buttons();
+        menu_cancel_sound.Play();
+        yield return new WaitForSeconds(seconds_to_next_screen);
+        Application.Quit(); // FIXME: might cause problems in web builds
+    }
+
+    private IEnumerator load_credits()
+    {
+        deactivate_all_buttons();
+        menu_validate_sound.Play();
+        yield return new WaitForSeconds(seconds_to_next_screen);
+
+        // SceneManager.LoadScene("Credits", LoadSceneMode.Single); // TODO: UNCOMMENT THIS TO LOAD CREDITS WHEN THE BUTTON IS PRESSED!
+    }
+
+    private IEnumerator load_new_game()
+    {
+        deactivate_all_buttons();
+        menu_validate_sound.Play();
+        yield return new WaitForSeconds(seconds_to_next_screen);
+
+        SceneManager.LoadScene("Main", LoadSceneMode.Single);
+    }
 
 }
