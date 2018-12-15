722bc8ac9223764ffdb93c1fb942f52e1fb9fde0
McFunkypants <breakdance@mcfunkypants.com>
2018-12-15 09:40:02 -0800

    Main Menu buttons regain focus when empty space clicked (so keyboard always works)

---
+++ Assets/MainMenuSelectPlayButton.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MainMenuSelectPlayButton : MonoBehaviour {
+
+    public GameObject SelectMe;
+    
+    // Use this for initialization
+	void Start () {
+        if (SelectMe)
+        {
+            EventSystem.current.SetSelectedGameObject(SelectMe, null);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void MainMenuClickEmptySpace()
+    {
+        Debug.Log("Main Menu: Clicked Empty Space: giving focus to play button");
+        if (SelectMe)
+        {
+            EventSystem.current.SetSelectedGameObject(SelectMe, null);
+        }
+    }
+}
