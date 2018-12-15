433271d37105557b43668d88169fce323b4380ca
A. Joël Lamotte (Klaim) <mjklaim@gmail.com>
2018-12-15 17:07:58 +0100

    TwistyTwisting behavior is now parametrable.

--- Assets/Scripts/rlc/movement/TwistyTwisting.cs
+++ Assets/Scripts/rlc/movement/TwistyTwisting.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class TwistyTwisting : MonoBehaviour {
 
+    public float time_between_rotation_changes = 0f;
+    public Vector3 min_rotation = new Vector3(1f, 1f, 1f);
+    public Vector3 max_rotation = new Vector3(1000f, 1000f, 1000f);
+
+
+    private Vector3 current_rotation;
+    private float next_rotation_change_time = 0f;
+
     // Use this for initialization
     void Start () {
 
@@ -11,7 +19,22 @@ public class TwistyTwisting : MonoBehaviour {
 
     // Update is called once per frame
     void Update () {
-        // Copy pasted from the "temporary" death animation of the laser cross, then tweaked to be realy random.
-        transform.rotation = transform.rotation * Quaternion.Euler(Random.Range(1.0f, 1000.0f) * Time.deltaTime, Random.Range(1.0f, 1000.0f) * Time.deltaTime, Random.Range(1.0f, 1000.0f) * Time.deltaTime);
+
+        var now = Time.time;
+        if (now >= next_rotation_change_time)
+        {
+            current_rotation = random_rotation();
+            next_rotation_change_time = now + time_between_rotation_changes;
+        }
+
+        transform.rotation = transform.rotation * Quaternion.Euler(current_rotation * Time.deltaTime);
+    }
+
+    private Vector3 random_rotation()
+    {
+        var rotation_x = Random.Range(min_rotation.x, max_rotation.x);
+        var rotation_y = Random.Range(min_rotation.y, max_rotation.y);
+        var rotation_z = Random.Range(min_rotation.z, max_rotation.z);
+        return new Vector3(rotation_x, rotation_y, rotation_z);
     }
 }
